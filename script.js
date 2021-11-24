266cae3fd7ac4399f9424496099bbf3b1c8b36c5
Sergey <shesergey2001@gmail.com>
2021-11-24 12:30:36 +1000

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Hello lodik");
+
+let array = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log("lodash version: ", _.VERSION);
+console.log("array: ", array);
+console.log("first elem: ", _.first(array));
+console.log("drop first 3 elements: ", _.drop(array, 3));
